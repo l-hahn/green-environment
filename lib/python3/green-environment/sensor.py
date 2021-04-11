2c6d0feb5cdcf8f51f173623f61428f400992831
l-hahn <lhahn@data-learning.de>
2021-04-11 09:30:51 +0200

    Use function annotations (PEP-3107). Remove type error raises.

--- lib/python3/green-environment/sensor.py
+++ lib/python3/green-environment/sensor.py
@@ -4,16 +4,11 @@ import time
 
 class giesomat:
 
-    def default_functor(values, **kwargs):
-        if type(values) not in (int, float, list):
-            raise ValueError("values not a list or number")
-        if type(values) == list:
-            for value in values:
-                if type(value) not in (int, float):
-                    raise ValueError("GPIO needs to be integer!")
-            print("\t".join(str(value) for value in values), **kwargs)
-        else:
+    def default_functor(values: "list of ints" or int, **kwargs):
+        if type(values) != list:
             print(values, **kwargs)
+        else:
+            print("\t".join(str(value) for value in values), **kwargs)
 
     def __init__(
             self, gpio, pulse=20, sample_rate=5,
@@ -25,37 +20,25 @@ class giesomat:
         self._pin_mask = 0
         self.reset()
 
-    def set_gpio(self, gpio):
-        if type(gpio) not in (int, list):
-            raise ValueError("GPIO not a list or integer")
-        if type(gpio) == list:
-            for gpio_pin in gpio:
-                if type(gpio_pin) != int:
-                    raise ValueError("GPIO needs to be integer!")
+    def set_gpio(self, gpio: "list of ints" or int):
         self._gpio = gpio if type(gpio) == list else [gpio]
 
     def gpio(self):
         return [gpio_pin for gpio_pin in self._gpio]
 
-    def set_pulse(self, pulse):
-        if type(pulse) not in (int, float):
-            raise ValueError("Pulse needs to be integer!")
+    def set_pulse(self, pulse: int):
         self._pulse = pulse
 
     def pulse(self):
         return self._pulse
 
-    def set_sample_rate(self, sample_rate):
-        if type(sample_rate) not in (int, float):
-            raise ValueError("Sample rate needs to be integer or float!")
+    def set_sample_rate(self, sample_rate: int or float):
         self._sample_rate = sample_rate
 
     def sample_rate(self):
         return self._sample_rate
 
-    def set_callback(self, call_back_id):
-        if type(call_back_id) != int:
-            raise ValueError("Call_back_id needs to be integer!")
+    def set_callback(self, call_back_id: int):
         self._call_back_id = call_back_id
 
     def callback(self):
@@ -81,10 +64,8 @@ class giesomat:
         for callback in self._call_backs:
             callback.reset_tally()
 
-    def get(self, iteration):
+    def get(self, iteration: int):
         iteration_values = []
-        if type(iteration) != int:
-            raise ValueError("Iteration needs to be integer!")
         for counter in range(iteration):
             values = [call.tally() for call in self._call_backs]
             for call in self._call_backs:
@@ -93,9 +74,7 @@ class giesomat:
             iteration_values.append(values)
         return iteration_values
 
-    def run(self, iteration=1, functor=default_functor, **kwargs):
-        if type(iteration) != int:
-            raise ValueError("Iteration needs to be integer!")
+    def run(self, iteration: int=1, functor=default_functor, **kwargs):
         while iteration != 0:
             if iteration > 0:
                 iteration -= 1
